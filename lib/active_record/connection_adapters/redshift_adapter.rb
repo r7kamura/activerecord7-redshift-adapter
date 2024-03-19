6bea9c3fd6cf2c03b948a76f2fe87d4ba4ed00e3
Quentin de Metz <quentin@de.me.tz>
2024-03-19 09:15:29 +0100

    require pg explicitly and bump version

--- lib/active_record/connection_adapters/redshift_adapter.rb
+++ lib/active_record/connection_adapters/redshift_adapter.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'pg'
+
 if ActiveRecord.version >= Gem::Version.new('7.1.0')
   require_relative 'redshift_7_1_adapter'
 elsif ActiveRecord.version >= Gem::Version.new('7.0.0')
